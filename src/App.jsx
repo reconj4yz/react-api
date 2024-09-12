7d94d6f398205d9caec41af28ce47bf6701b19a6
reconjayz <jaichawla263@gmail.com>
2024-09-12 20:33:53 +0530

    added api and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [products, setProducts] = useState([])
+  const getproducts = () => {
+    const api = "https://fakestoreapi.com/products";
+    axios
+      .get(api) 
+      .then((products) => {
+        console.log(products)
+        setProducts(products.data)
+      }
+      )
+      .catch((err) => console.log(err));
+      
+  };
+  const addproducts = () => {
+    const api = "https://fakestoreapi.com/products";
+    axios
+      .post(api,{
+        title: 'test product',
+        price: 13.5,
+        description: 'lorem ipsum set',
+        image: 'https://i.pravatar.cc',
+        category: 'electronic'
+      })
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+  console.log(products)
   return (
-    <div className='bg-sky-400'>App</div>
-  )
-}
+    <div className="bg-zinc-200 h-screen w-full">
+      <button className="px-3 py-1 bg-red-300 m-2" onClick={getproducts}>
+        Get Products
+      </button>
+      <button className="px-3 py-1 bg-red-300 m-2" onClick={addproducts}>
+        Add Products
+      </button>
+      <hr className="my-10"/>
+      <ul>
+        {products.length > 0 ?  products.map(product => <li className="px-3 py-1 bg-red-300 m-2 w-1/2">{product.title}</li>): <h1>Loading.....</h1>}
+        
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
